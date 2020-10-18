375c7b5bb6831251ffa1ba08ca1dc682e9aab210
aLaix <alan.limagalvez@yahoo.com>
2020-10-18 13:21:14 -0700

    Added Major, Minor, 7, maj7 and m7 chords. Added Anki notes generation

--- generate_fretboards.py
+++ generate_fretboards.py
@@ -1,14 +1,32 @@
+import os
+
 import fretboard
 import pandas as pd
+
+SVGS_FOLDER_PATH = 'svgs'
 
 
 def main():
     chords = pd.read_csv('chords.csv', index_col='Chord', dtype='str')
-
+    notes = []
     for chord, row in chords.iterrows():
+        print(chord)
         positions, fingers = row
         svg = fretboard.UkuleleChord(positions=positions, fingers=fingers)
-        svg.save(f'{chord}.svg')
+        os.makedirs(SVGS_FOLDER_PATH, exist_ok=True)
+        filename = f'Uke_{chord}.svg'
+        svg.save(os.path.join(SVGS_FOLDER_PATH, filename))
+        chord_type = get_chord_type(chord)
+        notes.append((f'''<img src="{filename}">''', chord, chord_type))
+    notes_df = pd.DataFrame(notes)
+    notes_df.to_csv('notes.csv', header=False, index=False)
+
+
+def get_chord_type(chord):
+    chord_type = 'Basic'
+    if '7' in chord:
+        chord_type = 'Seventh'
+    return chord_type
 
 
 if __name__ == '__main__':
